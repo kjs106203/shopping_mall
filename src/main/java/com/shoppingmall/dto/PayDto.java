71d40ffb88d117a65caa7d8e0e9224c3a9785e06
kjs106203 <kjs106203@naver.com>
2024-10-23 22:09:02 +0900

    Join 테이블 DTO 추가

---
+++ src/main/java/com/shoppingmall/dto/PayDto.java
@@ -0,0 +1,59 @@
+package common.shopping_mall.src.main.java.com.shoppingmall.dto;
+
+public class PayDto {
+    private int o_no;
+    private String p_name;
+    private int c_amount;
+    private int c_price;
+
+    public PayDto() {}
+
+    public PayDto(int o_no, String p_name, int c_amount, int c_price) {
+        this.o_no = o_no;
+        this.p_name = p_name;
+        this.c_amount = c_amount;
+        this.c_price = c_price;
+    }
+
+    public int getO_no() {
+        return o_no;
+    }
+
+    public void setO_no(int o_no) {
+        this.o_no = o_no;
+    }
+
+    public String getP_name() {
+        return p_name;
+    }
+
+    public void setP_name(String p_name) {
+        this.p_name = p_name;
+    }
+
+    public int getC_amount() {
+        return c_amount;
+    }
+
+    public void setC_amount(int c_amount) {
+        this.c_amount = c_amount;
+    }
+
+    public int getC_price() {
+        return c_price;
+    }
+
+    public void setC_price(int c_price) {
+        this.c_price = c_price;
+    }
+
+    @Override
+    public String toString() {
+        return "PayDto{" +
+                "o_no=" + o_no +
+                ", p_name='" + p_name + '\'' +
+                ", c_amount=" + c_amount +
+                ", c_price=" + c_price +
+                '}';
+    }
+}
