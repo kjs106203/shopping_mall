39508438e605f9a34c2afc689f6dde4df7848174
kjs106203 <kjs106203@naver.com>
2024-11-17 16:14:12 +0900

    Spring Boot 생성 후 실행 테스트 및 thymeleafe 활용 후 테스트 진행

---
+++ Shopping_web/src/test/java/com/boot/shopping_web/ShoppingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.shopping_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
